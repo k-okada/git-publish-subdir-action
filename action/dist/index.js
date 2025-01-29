80cdf43aeb1fa25d8b3f028cdb03b85d30b1eb48
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2025-01-29 19:03:10 +0900

    use git-lfs

--- action/dist/index.js
+++ action/dist/index.js
@@ -415,6 +415,9 @@ const main = async ({ env = process.env, log, }) => {
         recursive: true,
         copySourceDirectory: false,
     });
+    log.log(`##[info] Use Git LFS`);
+    const lfs_track = await (0, exports.exec)(`git lfs track ./**`, { log, env: childEnv, cwd: REPO_TEMP });
+    log.log(lfs_track.stdout);
     await (0, exports.exec)(`git add -A .`, { log, env: childEnv, cwd: REPO_TEMP });
     const message = config.message
         .replace(/\{target\-branch\}/g, config.branch)
